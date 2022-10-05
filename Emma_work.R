085768613d6dc88667d21fe75fa969f14de23c75
emmajuliej <emmajulie@ucsb.edu>
2022-10-05 15:30:19 -0700

    updates

--- Emma_work.R
+++ Emma_work.R
@@ -8,5 +8,10 @@ library(readr)
 
 #1)
 #a. import data
+airbnb <- read_csv("assign_2-2.csv")
 
-airbnb <- read_csv("assign_2.csv")
+#b. view data
+view(airbnb)
+colnames(airbnb)
+
+#c. rename "neighbourhood"
