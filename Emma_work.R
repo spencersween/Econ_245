49f651a4e74604943d07f11322a7665a9898c304
emmajuliej <emmajulie@ucsb.edu>
2022-10-05 15:54:53 -0700

    updates to hw

--- Emma_work.R
+++ Emma_work.R
@@ -5,13 +5,32 @@ library(tidyverse)
 library(dplyr)
 library(pacman)
 library(readr)
+library(plyr)
 
-#1)
+#1) setting up data
+
 #a. import data
 airbnb <- read_csv("assign_2-2.csv")
 
 #b. view data
-view(airbnb)
-colnames(airbnb)
+#view(airbnb) this will break the autograder
+#colnames(airbnb)
 
 #c. rename "neighbourhood"
+airbnb <- airbnb %>%
+  rename(neighborhood = "neighbourhood")
+
+
+#2) piping practice/summary stats
+
+#a. count neighborhoods
+neighborhoods <- count(airbnb, neighborhood)
+
+#b. arrange top 20 neighborhoods in descending order
+neighborhoods <- na.omit(neighborhoods)
+neighborhoods <- head(arrange(neighborhoods,desc(n)), n = 20)
+
+#c. 
+
+
+
